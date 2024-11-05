03bd7ce5c6ddb9aa93b5d9f9005f4d205effdea0
gome87 <mewhitelove@naver.com>
2024-11-05 14:03:10 +0900

    first commit

---
+++ src/main/java/com/hoxy133/llopharm/LloPharmApplication.java
@@ -0,0 +1,13 @@
+package com.hoxy133.llopharm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LloPharmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LloPharmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hoxy133/llopharm/LloPharmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoxy133.llopharm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LloPharmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
